3ce83a59278890cff886159ae5088c29279276b4
mkyang <303055758@qq.com>
2014-12-28 19:47:34 +0800

    create test to Printer.printResult and add function printResult into Printer

--- __tests__/test_printer.js
+++ __tests__/test_printer.js
@@ -9,4 +9,14 @@ describe('printer', function() {
       expect(headText).toBe('welcome to play this game\nyou should input 4 number');
     });
   });
+
+  describe('.printResult', function() {
+    it('should return correct expect result is congratulations', function() {
+      var Printer = require('../src/model/printer');
+      var printer = new Printer('4A0B');
+
+      var resultText = printer.printResult();
+      expect(resultText).toBe('congratulations!');
+    });
+  });
 });
--- src/model/printer.js
+++ src/model/printer.js
@@ -1,10 +1,20 @@
-function Printer() {
-
+function Printer(result) {
+  this.result = result;
 }
 
 Printer.printHead = function() {
   var headText = 'welcome to play this game' + '\n' + 'you should input 4 number';
   return headText;
+};
+
+Printer.prototype.printResult = function () {
+  var resultText = '';
+  if(this.result === '4A0B') {
+    resultText = 'congratulations!';
+  } else {
+    resultText = 'sorry! you answer is' + this.result + '\nyou can try again!';
+  }
+  return resultText;
 };
 
 module.exports = Printer;
