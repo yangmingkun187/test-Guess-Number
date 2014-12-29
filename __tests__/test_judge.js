267aa818c48ccaa4219376f91e3a115fa3bf1a37
mkyang <303055758@qq.com>
2014-12-29 10:43:32 +0800

    reconsitution test_judge codes

--- __tests__/test_judge.js
+++ __tests__/test_judge.js
@@ -2,10 +2,16 @@ jest.dontMock('../src/model/judge');
 jest.dontMock('lodash');
 
 describe('judge', function() {
+  var Judge;
+  var _;
+
+  beforeEach(function() {
+    Judge = require('../src/model/judge');
+    _ = require('lodash');
+  });
+
   describe('#judgeResult', function() {
     it('should return correct string is 4A0B', function() {
-      var Judge = require('../src/model/judge');
-      var _ = require('lodash');
       var inputs = [1,2,3,4];
       var mathRands = [1,2,3,4];
 
@@ -17,8 +23,6 @@ describe('judge', function() {
     });
 
     it('should return correct string is 2A2B', function() {
-      var Judge = require('../src/model/judge');
-      var _ = require('lodash');
       var inputs = [1,2,4,3];
       var mathRands = [1,2,3,4];
 
@@ -30,8 +34,6 @@ describe('judge', function() {
     });
 
     it('should return correct string is 0A0B', function() {
-      var Judge = require('../src/model/judge');
-      var _ = require('lodash');
       var inputs = [5,6,7,8];
       var mathRands = [1,2,3,4];
 
@@ -43,8 +45,6 @@ describe('judge', function() {
     });
 
     it('should return correct string is 0A4B', function() {
-      var Judge = require('../src/model/judge');
-      var _ = require('lodash');
       var inputs = [4,3,2,1];
       var mathRands = [1,2,3,4];
 
